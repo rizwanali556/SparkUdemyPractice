34e673b08200c092f3f760c1ae828be3fd4eaeff
Rizwan <rizwan51.ali554@gmail.com>
2020-07-15 10:07:13 +0530

    commit the latest deploy scripts to git

---
+++ deploy/aws_deploy_properties.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+# Tag_Version number for the project
+export BuildName="version_$Tag_Version"
+
+# BucketName for the s3 Artifact
+export BucketName="s3-jenkins-artifact"
+
+# ApplicationName will be fetched based on jenkins job name or you can give your own job name as in git Repo
+export ApplicationName="$(pwd | cut -d'/' -f7)"
+
+# Source application path to copy Repositories from Github to S3
+export Applicationpath="$(pwd)"
+
+# Key name to specify the Project version folder in s3
+export keyname=""$ApplicationName"_"$BuildName"/"
+
+# Region name for deploying in to s3 Artifact
+export region="--region ap-south-1"
+
+# Export Artifacts for excluding artifacts/folders from Github Repo
+export exclude_variables="--exclude=project/* --exclude=.idea/* --exclude=.git/* --exclude=deploy/*"
